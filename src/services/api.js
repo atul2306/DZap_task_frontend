764f0f8fedd99bb572b4b9a7e05e5974a5415524
atul2306 <atulranjan2306@gmail.com>
2023-12-31 16:40:28 +0530

    deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3001/api';
+const apiUrl = process.env.REACT_APP_API;
 
 const api = {
   getCryptocurrencies: () => axios.get(`${apiUrl}/cryptocurrencies`),
